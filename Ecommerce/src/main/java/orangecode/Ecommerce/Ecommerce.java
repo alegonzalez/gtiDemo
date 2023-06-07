9e77252339c11e96aa4444c29c0bfe6fa4f37907
alegonzalez <alegonzalez21195@gmail.com>
2023-06-07 15:26:18 -0600

    Architect Asian development

--- Ecommerce/src/main/java/orangecode/Ecommerce/Ecommerce.java
+++ Ecommerce/src/main/java/orangecode/Ecommerce/Ecommerce.java
@@ -5,6 +5,12 @@ public class Ecommerce {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("This is a test with github");
+		
+		System.out.print("This is a test with github");
+		System.out.print("This is a test with Alejandro");
+		System.out.print("This is a test with test");
+		System.out.print("This is a test with I dont know");
+		
 	}
 
 }
